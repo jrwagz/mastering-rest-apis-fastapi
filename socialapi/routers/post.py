9ea84c4cb06741e0c90f549cc3052e270f6202f8
Justin Wagner <jrwagz@gmail.com>
2024-05-07 21:54:55 -0600

    Add comments to posts.  End Chapter 2

--- socialapi/routers/post.py
+++ socialapi/routers/post.py
@@ -1,14 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from socialapi.models.post import UserPost, UserPostIn
+from socialapi.models.post import (
+    Comment,
+    CommentIn,
+    UserPost,
+    UserPostIn,
+    UserPostWithComments,
+)
 
 router = APIRouter()
 
 
 post_table = {}
+comment_table = {}
 
 
-@router.post("/post")
+@router.post("/post", status_code=201)
 async def create_post(post: UserPostIn) -> UserPost:
     data = post.model_dump()
     last_record_id = len(post_table)
@@ -20,3 +27,36 @@ async def create_post(post: UserPostIn) -> UserPost:
 @router.get("/post")
 async def list_posts() -> list[UserPost]:
     return list(post_table.values())
+
+
+def find_post(post_id: int) -> UserPost:
+    post = post_table.get(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail=f"Post {post_id} not found.")
+    return post
+
+
+@router.post("/comment", status_code=201)
+async def create_comment(comment: CommentIn) -> Comment:
+    find_post(post_id=comment.post_id)
+    data = comment.model_dump()
+    last_record_id = len(comment_table)
+    new_comment = Comment(**data, id=last_record_id)
+    comment_table[last_record_id] = new_comment
+    return new_comment
+
+
+@router.get("/post/{post_id}/comment")
+async def get_post_comments(post_id: int) -> list[Comment]:
+    find_post(post_id=post_id)
+    post_comments = [c for c in comment_table.values() if c.post_id == post_id]
+    return post_comments
+
+
+@router.get("/post/{post_id}")
+async def get_post_with_comments(post_id: int) -> UserPostWithComments:
+    post = find_post(post_id=post_id)
+    return UserPostWithComments(
+        post=post,
+        comments=await get_post_comments(post_id=post_id),
+    )
